4f20da3ac104b0e165f12941b7cdf9855bd2d87f
mspcity <codepark2019@gmail.com>
2020-06-07 23:37:05 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+
+  end
+
+  def match(array)
+    @word.split("")
+    array.select do |element|
+    if word.split("").sort == element.split("").sort
+      element
+    end
+    end
+  end
+end
